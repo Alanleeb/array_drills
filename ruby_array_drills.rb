d33e460674c1cb8cf357a5dcbcf337740bb3e956
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-18 16:12:35 -0600

    do each array method

--- ruby_array_drills.rb
+++ ruby_array_drills.rb
@@ -1,7 +1,18 @@
-puts "outputting from an array via the index using the syntax (puts Array[]"
+puts "outputting from an array via the index using the syntax (puts Array[])"
 arr = [0,9,4,5,6,8,2,34]
 puts arr
 puts "puts arr[3] prints out index 3 which is #{arr[3]}"
 arrStr = ['cats', 'dogs', 'cows', 'horses']
 puts arrStr
-puts "puts arr[1] prints out index 3 which is #{arr[1]}"
+puts "puts arr[1] prints out index 1 which is #{arrStr[1]}"
+puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+puts 'outputting every element in an array frmo the beginning'
+puts "The Syntax is:  |  Array.each do |anything|   |"
+puts "                |  puts anything              |"
+puts "                |  end                        |"
+puts "                 -----------------------------"
+arr2 = ['baseball', 'basketball', 'soccer', 'hockey']
+arr2.each do |sport|
+  puts sport
+end
+puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
