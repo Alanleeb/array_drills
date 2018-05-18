101d2f88ef28b73bbd097b90839b6a3769f2f9c0
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-18 15:58:35 -0600

    initial push

---
+++ ruby_array_drills.rb
@@ -0,0 +1,7 @@
+puts "outputting from an array via the index using the syntax (puts Array[]"
+arr = [0,9,4,5,6,8,2,34]
+puts arr
+puts "puts arr[3] prints out index 3 which is #{arr[3]}"
+arrStr = ['cats', 'dogs', 'cows', 'horses']
+puts arrStr
+puts "puts arr[1] prints out index 3 which is #{arr[1]}"
